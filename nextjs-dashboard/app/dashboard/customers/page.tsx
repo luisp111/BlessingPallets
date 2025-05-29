092fe46fa47d82eb4a659d93f64411826b7ec022
Luisp111 <123522853+luisp111@users.noreply.github.com>
2025-05-29 16:36:11 -0700

    Starting conversion from tutorial to actual site for Blessing Pallets

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,3 +1,13 @@
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchCardData } from '@/app/lib/data';
+import { Suspense } from 'react';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 export default function Page() {
-    return <p>Customers Page</p>;
+    return <div>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Blessing Pallets
+      </h1>
+      
+    </div>;
   }
