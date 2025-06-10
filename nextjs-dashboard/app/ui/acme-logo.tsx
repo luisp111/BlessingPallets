f4a257d99580d54b80219a3e2c1f756cd131bbf9
Luisp111 <123522853+luisp111@users.noreply.github.com>
2025-06-10 16:14:43 -0700

    Adding new logo

--- nextjs-dashboard/app/ui/acme-logo.tsx
+++ nextjs-dashboard/app/ui/acme-logo.tsx
@@ -1,13 +1,18 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={`${lusitana.className} flex items-center justify-center text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[30px]">Blessing Pallets</p>
+      <Image
+        src="/gallery/Logo/Logo.jpg"
+        alt="Blessing Pallets Logo"
+        width={120} // Try 120–160 if needed
+        height={120}
+        className="w-auto h-auto"
+      />
     </div>
   );
 }
